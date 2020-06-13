ab0f41f8be125e99eb5d6ce911e054ddee724ca0
amitchakor21 <amichakor21@gmail.com>
2020-06-13 18:38:07 +0530

    13 June

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { HeaderComponent } from './home/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SlideshowComponent } from './home/slideshow/slideshow.component';
 import { CardComponent } from './home/card/card.component';
+import { AboutComponent } from './home/about/about.component';
+import { TimelineComponent } from './home/timeline/timeline.component';
 
 
 @NgModule({
@@ -17,15 +20,20 @@ import { CardComponent } from './home/card/card.component';
     HeaderComponent,
     HomeComponent,
     SlideshowComponent,
-    CardComponent
+    CardComponent,
+    AboutComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbCarouselModule,
-    
+    MDBBootstrapModule.forRoot()
+
     ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  HostListener, ElementRef  } from '@angular/core';
+import { IconsModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+import { Renderer2, Inject } from '@angular/core';
+// import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  logo="/assets/images/logo.png";
-  constructor() { }
+logo = '/assets/images/logo.png';
+front = '/assets/images/frontnight.jpg';
+frontLeft = '/assets/images/frontLeft.jpg';
+frontRight = '/assets/images/frontRight.jpg';
+
+  topPosToStartShowing = 40;
+  isShow: boolean;
+  scrolledDown: boolean;
+  shand = document.getElementsByClassName('frontImage') as HTMLCollectionOf<HTMLElement>;
+  ap = document.getElementsByClassName('trial') as HTMLCollectionOf<HTMLElement>;
+
+  constructor(private elementRef: ElementRef){}
 
   ngOnInit(): void {
+    // const s = this.renderer2.createElement('script');
+    // s.type = 'text/javascript';
+    // s.src = 'https://path/to/your/script';
+    // s.text = ``;
+    // this.renderer2.appendChild(this._document.body, s);
+    this.ap[0].style.color = 'red';
+
   }
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+
+    // this.shand[0].style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
+    // this.shand[1].style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
+
+
+    // this.elementRef.nativeElement.ownerDocument.body.style.transform = '#EDF5E1';
+    // console.log('[scroll]', scrollPosition);
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+      this.scrolledDown = true;
+      // console.log("threshold crossed");
+    } else {
+      this.isShow = false;
+      this.scrolledDown = false;
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    const mywidth = window.innerWidth;
+    if (mywidth < 768){
+      // document.getElementById('horizontal').style.display = 'none';
+      // document.getElementById('vertical').style.display = 'block';
+    }
+    else{
+      // document.getElementById('vertical').style.display = 'none';
+      // document.getElementById('horizontal').style.display = 'block';
+    }
+  }
+
+  // scroll(str: string) {
+  //  const one = new  DOMParser().parseFromString(str, "text/xml");
+
+  //   el:HTMLElement = 'trial';
+  //   el.scrollIntoView({behavior: 'smooth'});
+  // }
+
+  scroll(str: string){
+    const el = document.getElementById(str);
+    el.scrollIntoView({behavior:'smooth'})
+  }
+
 
 }
