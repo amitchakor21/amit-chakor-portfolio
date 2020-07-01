ef865b88022ab9352bdb3af8e8cba9b1e2d11930
amitchakor21 <amichakor21@gmail.com>
2020-07-01 21:48:14 +0530

    1july2020

--- src/app/home/card/card.component.ts
+++ src/app/home/card/card.component.ts
@@ -1,56 +1,151 @@
-import { Component, OnInit, HostListener,AfterContentInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit,AfterContentInit {
-
+export class CardComponent implements OnInit, AfterViewInit {
   logo = '/assets/images/logo.png';
-  amit = `asdalds`;
-  shand = document.getElementsByClassName('shade') as HTMLCollectionOf<HTMLElement>;
-  shandOut = document.getElementsByClassName('shadeOut') as HTMLCollectionOf<HTMLElement>;
+  shand = document.getElementsByClassName('shade') as HTMLCollectionOf<
+    HTMLElement
+  >;
+  shandOut = document.getElementsByClassName('shadeOut') as HTMLCollectionOf<
+    HTMLElement
+  >;
 
-  
-  constructor() { }
+  imageMap1 = { width: 827, 
+                height: 413, 
+                x:1,
+                y:1,
+                maps: [
+                    {points:2,alt:"google",coords:[148,251,28,156],href:"www.google.com",shape:'rect'},
+                    {points:2,alt:"facebook",coords:[279,158,439,253],href:"www.facebook.com",shape:'rect'},
+                    {points:6,alt:"insta",coords:[477,191,475,237,542,236,564,158,524,144,512,187],href:"www.instagram.com",shape:'poly'},
+                    {points:4,alt:"insta2",coords:[479,296,478,386,582,386,581,297],href:"www.instagram.com",shape:'poly'}
+                ] };
 
-  ngAfterContentInit():void{
-    this.onResize()
+  that=this;
+
+                
+  constructor() {
+  }
+
+
+
+  ngAfterViewInit(): void {
+
+    // this.renderImageMap(this.imageMap1,"firstimage")
+    // setTimeout(function(){ that.renderImageMap.bind(this,this.imageMap1,"firstimage")},2000)
+
   }
 
   ngOnInit(): void {
+      // this.renderImageMap(this.imageMap1,"firstimage")   
 
-    // console.log(this.shand[0].offsetHeight)
-    // console.log(this.shand[1].offsetHeight)
-    // console.log(this.shand[2].offsetHeight)
-    // this.shand[0].s
-    
-  }
+    }
+  
   @HostListener('window:scroll')
-  onScroll(){
-    if(scrollY>200 && scrollY<250)
-    {
-      this.onResize()
+  onScroll() {
+    if (scrollY > 200 && scrollY < 250) {
+      this.onResize();
     }
   }
 
   @HostListener('window:resize')
   onResize() {
-    var maxHeight=0;
+    this.renderImageMap(this.imageMap1,"firstimage")   
+    var maxHeight = 0;
     var max1;
-    for(let i=0;i<3 ;i++){
-      maxHeight= this.shandOut[i].offsetHeight > maxHeight ? this.shandOut[i].offsetHeight : maxHeight;
+    for (let i = 0; i < 3; i++) {
+      maxHeight =
+        this.shandOut[i].offsetHeight > maxHeight
+          ? this.shandOut[i].offsetHeight
+          : maxHeight;
     }
-    max1=maxHeight+100+20+15;
+    max1 = maxHeight + 100 + 20 + 15;
 
-      for(let i=0;i<3 ;i++){
-        this.shand[i].style.height= max1+'px';
-      }
+    for (let i = 0; i < 3; i++) {
+      this.shand[i].style.height = max1 + 'px';
+    }
 
     // console.log(max1);
-    max1=0;
+    max1 = 0;
+
   }
 
-  
+  onLoad()
+  {
+
+    this.renderImageMap(this.imageMap1,"firstimage")   
+
+  }
+
+
+  renderImageMap(imgMap,id){
+    const imgEl= document.getElementById(id)
+    let img= document.getElementById("myimage123");
+    // console.log(img.clientHeight)
+    // console.log(img.clientWidth) 
+
+
+    let newEl="";
+    imgMap.x=img.clientWidth/imgMap.width;    //change tj]his afterwards
+    imgMap.y=img.clientHeight/imgMap.height;
+    // console.log(imgMap.x)
+    // console.log(imgMap.y)
+    let text="";
+
+    for(let map of imgMap.maps){
+      if(map.shape === "rect")
+      {
+        text="";
+        for(let i=0;i<4;i++){
+          if(i%2 == 0){
+            text = text + imgMap.x * map.coords[i] ;
+          }
+          else{
+            text = text + imgMap.y * map.coords[i];
+          }
+          if(i!=3){
+            text = text + ',';
+          }
+        }
+        // console.log(text);
+        
+        newEl = newEl + ` <area alt="${map.maps}" href="${map.href}" coords="${text}" shape="${map.shape}">`
+      }
+      else if(map.shape === "poly")
+      {
+        text="";
+        for(let i=0;i<(map.points*2);i++){
+          if(i%2 == 0){
+            text = text + imgMap.x * map.coords[i] ;
+          }
+          else{
+            text = text + imgMap.y * map.coords[i];
+          }
+          if(i != (map.points*2-1)){
+            text = text + ',';
+          }
+        }
+        // console.log(text);
+        
+        newEl = newEl + ` <area target="" alt="${map.maps}" href="${map.href}" coords="${text}" shape="${map.shape}" style="border:1px red;">`
+      }
+
+      else{
+        console.log("unable to find shape");
+      }
+
+    }
+    imgEl.innerHTML=newEl;
+  }
+
 }
+
--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit,  HostListener, ElementRef  } from '@angular/core';
+import { Component, OnInit,  HostListener, ElementRef  , AfterViewInit } from '@angular/core';
 import { IconsModule } from 'angular-bootstrap-md';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 import { Renderer2, Inject } from '@angular/core';
+import { NgbButtonLabel } from '@ng-bootstrap/ng-bootstrap';
+import { MinLengthValidator } from '@angular/forms';
 // import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
@@ -11,76 +13,125 @@ import { Renderer2, Inject } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
-logo = '/assets/images/logo.png';
-front = '/assets/images/frontnight.jpg';
-frontLeft = '/assets/images/frontLeft.jpg';
-frontRight = '/assets/images/frontRight.jpg';
-
+  logo = '/assets/images/logo.png';
+  front = '/assets/images/frontnight.jpg';
+  frontLeft = '/assets/images/frontLeft.jpg';
+  frontRight = '/assets/images/frontRight.jpg';
+  lastscroll = 0;
+  direction ="up"
+  header;
   topPosToStartShowing = 40;
   isShow: boolean;
   scrolledDown: boolean;
   shand = document.getElementsByClassName('frontImage') as HTMLCollectionOf<HTMLElement>;
   ap = document.getElementsByClassName('trial') as HTMLCollectionOf<HTMLElement>;
+  scrollPosition: number;
+  listitems=<HTMLElement[]><any>document.querySelectorAll('.link-items');
+  linkItemsOut=1
+  constructor(private elementRef: ElementRef){
+  }
 
-  constructor(private elementRef: ElementRef){}
+  ngAfterViewInit(): void {
+    this.header= document.getElementById('header');
+    this.header.style.height=`100px`;
+    this.listitems=<HTMLElement[]><any>document.querySelectorAll('.link-items');
+    this.scrollPosition=0
+    this.header = document.getElementById('header');
+    // this.AdjustHeaderPadding()
+  }
 
   ngOnInit(): void {
-    // const s = this.renderer2.createElement('script');
-    // s.type = 'text/javascript';
-    // s.src = 'https://path/to/your/script';
-    // s.text = ``;
-    // this.renderer2.appendChild(this._document.body, s);
-    this.ap[0].style.color = 'red';
-
   }
 
   @HostListener('window:scroll')
   checkScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
+    if(this.scrollPosition>300)
+      this.linkItemsOut=0
+    else 
+      this.linkItemsOut=1
 
-    // this.shand[0].style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
-    // this.shand[1].style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
-
-
-    // this.elementRef.nativeElement.ownerDocument.body.style.transform = '#EDF5E1';
-    // console.log('[scroll]', scrollPosition);
-    if (scrollPosition >= this.topPosToStartShowing) {
-      this.isShow = true;
-      this.scrolledDown = true;
-      // console.log("threshold crossed");
-    } else {
-      this.isShow = false;
-      this.scrolledDown = false;
+    if(this.scrollPosition<300){
+      // this.AdjustHeaderPadding()
     }
+
+    if(this.scrollPosition<200)
+    {      
+      this.ShowHeader(1);
+      this.lastscroll= this.scrollPosition
+      return                              //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    }    
+    
+    this.checkDir()
+
+    if(this.direction === 'up')
+      this.ShowHeader(1)
+    else
+      this.ShowHeader(1)
+
   }
-
-  @HostListener('window:resize')
-  onResize() {
-    const mywidth = window.innerWidth;
-    if (mywidth < 768){
-      // document.getElementById('horizontal').style.display = 'none';
-      // document.getElementById('vertical').style.display = 'block';
-    }
-    else{
-      // document.getElementById('vertical').style.display = 'none';
-      // document.getElementById('horizontal').style.display = 'block';
-    }
-  }
-
-  // scroll(str: string) {
-  //  const one = new  DOMParser().parseFromString(str, "text/xml");
-
-  //   el:HTMLElement = 'trial';
-  //   el.scrollIntoView({behavior: 'smooth'});
-  // }
 
   scroll(str: string){
     const el = document.getElementById(str);
     el.scrollIntoView({behavior:'smooth'})
   }
 
+  checkDir(){
+    if((this.lastscroll < this.scrollPosition) && this.direction != "down"){     
+      this.direction = "down"
+    }
+    else if ((this.lastscroll > this.scrollPosition) && this.direction != "up"){
+      this.direction = "up"
+     }
+    this.lastscroll= this.scrollPosition
+  }
+
+
+
+  ShowHeader(display){
+    if(!display)
+    {
+      this.header.style.visibility = 'hidden';
+    }else //if ( && !display) 
+    {
+      this.header.style.visibility = 'visible';
+    }
+  }
+
+  AdjustHeaderPadding()
+  {
+    // const bg=this.scrollPosition*255/300;
+    // const bg1= (bg<127)?255:0; 
+    if(this.scrollPosition<300)
+    {
+      
+        const padding= 1+ 0.8 - Math.min(0.8,this.scrollPosition/100);
+        this.listitems.forEach(element => {
+          element.style.padding=`0px ${padding}vw`;
+          // element.style.backgroundColor=`rgb(${bg},${bg},${bg})`
+          // element.style.color=`rgb(${bg1},${bg1},${bg1})`
+        });
+      const padding2= 1.5+ 3.5 - Math.min(3.5,this.scrollPosition/(100/3.5));
+      this.header.style.padding=`0px ${padding2}vw`;
+      
+    }
+
+
+    if(this.scrollPosition>100 && this.scrollPosition<300)
+    {
+      const height = 60 + 40 - Math.min(40,(this.scrollPosition-100)/2.5);
+      this.header.style.height=`${height}px`;
+    } 
+
+  }
+
 
 }
+
+
+  
+  
+
